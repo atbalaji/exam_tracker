bb6a81309765ea6228d3b8d3b4ff1ec882220eac
atbalaji <balaji2765@gmail.com>
2026-02-04 03:26:31 +0000

    - Default user migration

---
+++ db/migrate/20260204032533_add_demo_user.rb
@@ -0,0 +1,23 @@
+class AddDemoUser < ActiveRecord::Migration[7.1]
+  def up
+    password_digest = BCrypt::Password.create("1234")
+
+    execute <<~SQL
+      INSERT INTO users (name, email, password_digest, created_at, updated_at)
+      VALUES (
+        'Default User',
+        'default@example.com',
+        '#{password_digest}',
+        NOW(),
+        NOW()
+      )
+      ON CONFLICT (email) DO NOTHING;
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      DELETE FROM users WHERE email = 'default@example.com';
+    SQL
+  end
+end
