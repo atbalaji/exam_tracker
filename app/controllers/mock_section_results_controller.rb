eb9a42a2807a7b7b1558b4cedc3a9fae5afd358c
atbalaji <balaji2765@gmail.com>
2026-02-07 03:30:32 +0000

    - Added frontend and backend validations for mock sections results creation and edit

--- app/controllers/mock_section_results_controller.rb
+++ app/controllers/mock_section_results_controller.rb
@@ -1,19 +1,32 @@
 class MockSectionResultsController < ApplicationController
   def create
-    params[:results].each do |result|
-      next if result[:total_questions].blank? && result[:attempted].blank? && result[:correct].blank?
+    mock_attempt_id = params[:results].first[:mock_attempt_id]
 
-      mock_section_result = MockSectionResult.find_or_initialize_by(
-        mock_attempt_id: result[:mock_attempt_id],
-        section_id: result[:section_id],
-      )
+    ActiveRecord::Base.transaction do
+      params[:results].each do |result|
+        next if result[:total_questions].blank? && result[:attempted].blank? && result[:correct].blank?
 
-      mock_section_result.attempted = result[:attempted]
-      mock_section_result.correct = result[:correct]
+        mock_section_result = MockSectionResult.find_or_initialize_by(
+          mock_attempt_id: result[:mock_attempt_id],
+          section_id: result[:section_id],
+        )
 
-      mock_section_result.save!
+        mock_section_result.assign_attributes(
+          total_questions: result[:total_questions],
+          attempted: result[:attempted],
+          correct: result[:correct]
+        )
+
+        unless msr.save
+          raise ActiveRecord::Rollback, mock_section_result.errors.full_messages.join(", ")
+        end
+      end
     end
 
-    redirect_to mock_attempt_path(params[:results].first[:mock_attempt_id])
+
+    redirect_to mock_attempt_path(mock_attempt_id), notice: "Section results saved"
+
+    rescue => e
+      redirect_to mock_attempt_path(mock_attempt_id), alert: e.message.presence || "Invalid section data"
   end
 end
--- app/models/mock_section_result.rb
+++ app/models/mock_section_result.rb
@@ -2,9 +2,9 @@ class MockSectionResult < ApplicationRecord
   validates :mock_attempt_id, presence: true
   validates :section_id, presence: true, uniqueness: { scope: :mock_attempt_id }
 
-  validates :total_questions, numericality: { greater_than: 0 }
-  validates :attempted, numericality: { greater_than_or_equal_to: 0 }
-  validates :correct, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: :attempted }
+  validates :total_questions, presence: true, numericality: { greater_than: 0 }
+  validates :attempted, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :correct, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: :attempted }
 
   validate :logical_consistency
 
